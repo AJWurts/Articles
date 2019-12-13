8bc5a3f20b82be6557502f016ebb87fd740d83ef
Alexander Wurts <ajwurts@gmail.com>
2019-12-13 12:36:58 -0500

    Added Classes back to AdvPython

--- articleui/src/components/advpython/AdvPython.jsx
+++ articleui/src/components/advpython/AdvPython.jsx
@@ -22,44 +22,105 @@ export default function IntroPython(props) {
       </h1>
       <h3>By Alexander Wurts</h3>
       <p>
-          If you have any questions, comments, or think there is an error please email me at
+        If you have any questions, comments, or think there is an error please email me at
           <b> alexwurtsfeedback@gmail.com</b>.
       </p>
       <h4>
         This is where it gets fun.
       </h4>
       <p>
-        This tutorial works on top of the Intro Python tutorial. Make sure you read through that one first. 
+        This tutorial works on top of the Intro Python tutorial. Make sure you read through that one first.
       </p>
       <p>
         The order of this tutorial is less important than the intro tutorial. The first section, Classes and Objects, should be read first, and the rest can be read in any order.
       </p>
       <TableOfContents>
-      <Section link="classes" label="Classes and Objects">
-        
-      </Section>
-      <Section link="lists" label="List Processing">
-        
-      </Section>
-      <Section link='files' label="File Reading and Writing">
+        <SubSection link="syntax-classes" label="Classes">
+          <p>
+            Object orientated programming is a progrmaming method that organizes the program around
+            code and objects as opposed to functions and logic. Python is built on top of objects,
+            every object except for some primitive types are objects. Strings are objects built
+            ontop of lists, also objects, or chars (not objects).
+                            </p>
+          <p>
+            Classes are used to define objects. A class can contains variables and methods
+            (functions in a class). Methods are special because they have access to the object
+            variables. Therefore it is common for a method to have no parameters because it can use the object variables to do calculations.
+                            </p>
+          <Code.Wrapper>
+            <Code.Title>
+              Example Dog Class
+            </Code.Title>
+            <Code.Code>
+              <Code.Line tabs={0}>
+                class Dog():
+              </Code.Line>
+              <Code.Line tabs={1}>
+                def __init__(self, name, human_years, breed):
+              </Code.Line>
+              <Code.Line tabs={2}>
+                self.name = name
+               </Code.Line>
+              <Code.Line tabs={2}>
+                self.dog_years = human_years * 7
+              </Code.Line>
+              <Code.Line tabs={2}>
+                self.breed = breed
+              </Code.Line>
+              <br />
+              <Code.Line tabs={1}>
+                def pet(self):
+              </Code.Line>
+              <Code.Line tabs={2}>
+                print(name, "wags their tail.")
+              </Code.Line>
+              <br />
+              <Code.Line tabs={1}>
+                def feed(self, food_amount):
+              </Code.Line>
+              <Code.Line tabs={2}>
+                print(name, "eats")
+              </Code.Line>
+              <br />
+              <Code.Line tabs={1}>
+                def getBreed(self):
+              </Code.Line>
+              <Code.Line tabs={2}>
+                return self.breed
+              </Code.Line>
+              <br />
+              <Code.Line tabs={1}>
+                def setAge(self, human_years):
+              </Code.Line>
+              <Code.Line tabs={2}>
+                self.dog_years = human_years * 7
+                </Code.Line>
+              <br />
+            </Code.Code>
+          </Code.Wrapper>
+        </SubSection>
+        <Section link="lists" label="List Processing">
 
-      </Section>
-      <Section link="mapandfilter" label="Map and Filter using Lambda">
-        
-      </Section>    
-      <Section link="yield" label="Iterators">
+        </Section>
+        <Section link='files' label="File Reading and Writing">
 
-      </Section>
-      <Section link="importing" label="Downloading Modules with PyPI">
+        </Section>
+        <Section link="mapandfilter" label="Map and Filter using Lambda">
 
-      </Section>
-      <Section link="sets" label="Sets">
+        </Section>
+        <Section link="yield" label="Iterators">
 
-      </Section>
-      <Section link="nextsteps" label="Next Steps">
+        </Section>
+        <Section link="importing" label="Downloading Modules with PyPI">
 
-      </Section>
-      {/* <Section link="nextsteps" label="Next Steps">
+        </Section>
+        <Section link="sets" label="Sets">
+
+        </Section>
+        <Section link="nextsteps" label="Next Steps">
+
+        </Section>
+        {/* <Section link="nextsteps" label="Next Steps">
           <p>
             You now have a good idea of the basic components of Python. Try to work through the easy projects above and if you feel good go for the medium projects. Move on to the Advanced Python Tutorial (Coming Soon!) to learn more about what Python can do. 
           </p>
@@ -175,6 +236,6 @@ export default function IntroPython(props) {
           </ul>
         </Section> */}
       </TableOfContents>
-      </div>
+    </div>
   )
 }
